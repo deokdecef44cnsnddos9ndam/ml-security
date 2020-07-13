30215c593591a4e022fa6bff4d0e2370515e36e5
deokdecef44cnsnddos9ndam@gmail.com <deokdecef44cnsnddos9ndam@gmail.com>
2020-07-13 16:03:31 -0400

    added utils module

---
+++ utils.py
@@ -0,0 +1,46 @@
+import torch
+import matplotlib.pyplot as plt
+from skimage import io
+import numpy as np
+
+# psuedo-types for illustration
+Image = Images = torch.FloatTensor
+Logit = Logits = torch.FloatTensor
+Gradient = Gradients = torch.FloatTensor
+
+def load_image_as_tensor(image_path: str) -> Image:
+    """
+    Loads an image from image_path into a tensor
+
+    Parameters
+    ----------
+    image_path: str
+        Path to an image
+
+    Returns
+    -------
+        Image
+    """
+    img = io.imread(image_path)
+    if len(img.shape) == 2:
+        img = np.tile(np.expand_dims(img, -1), [1, 1, 3])
+    img = torch.FloatTensor(img)
+    img = img.permute(2, 0, 1)
+    img = img.unsqueeze(0)
+    return img / 255.0
+
+def show(img: Image):
+    """
+    Displays the image in the notebook window
+    
+    Params
+    ------
+    img : Image
+    """
+    img = img.detach()
+    img = img[0].numpy()
+    img = img.transpose(1, 2, 0)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.imshow(img, interpolation='nearest', aspect='equal')
+    ax.set_xticks([], minor=[])
+    ax.set_yticks([], minor=[])
