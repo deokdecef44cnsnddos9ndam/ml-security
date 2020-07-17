08cf5494292ae65cd46313353e510e6892b08334
deokdecef44cnsnddos9ndam <68249075+deokdecef44cnsnddos9ndam@users.noreply.github.com>
2020-07-17 14:42:06 -0400

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -3,7 +3,23 @@
 import torch
 import torch.nn as nn
 
+class Binarize(nn.Module):
 
+  def forward(self, x):
+    new_x = torch.zeros_like(x)
+    new_x[x > 0.5] = 1.0
+    new_x[x <= 0.5] = 0.0
+    return new_x
+  
+class PutOnDevice(nn.Module):
+  
+  def __init__(self, device):
+    super().__init__()
+    self.device = device
+  
+  def forward(self, x):
+    return x.to(self.device)
+  
 def build_model(device):
   model = nn.Sequential(
     nn.Conv2d(1, 16, kernel_size=5),
@@ -20,3 +36,30 @@ def build_model(device):
   )
   model = model.to(device)
   return model
+
+def get_training_data(device):
+  train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('/files/', 
+                             train=True,
+                             download=True,
+                             transform=torchvision.transforms.Compose([
+                                torchvision.transforms.ToTensor(),
+                                Binarize(),
+                                PutOnDevice(device),
+                              ])),
+                             batch_size=128, 
+                             shuffle=True)
+  return train_loader
+def get_testing_data(device):
+  test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('/files/', 
+                             train=False, 
+                             download=True,
+                             transform=torchvision.transforms.Compose([
+                                torchvision.transforms.ToTensor(),
+                                Binarize(),
+                                PutOnDevice(device),
+                             ])),
+                             batch_size=128, 
+                             shuffle=True)
+  return test_loader
