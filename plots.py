7e274ba7241c0dbdc1ca32496a59d6a2edc3fecd
deokdecef44cnsnddos9ndam <68249075+deokdecef44cnsnddos9ndam@users.noreply.github.com>
2020-07-21 11:18:33 -0400

    Update plots.py

--- plots.py
+++ plots.py
@@ -99,7 +99,7 @@ def vis_probs(ax, probs, labels=None):
         ax.set_ylim(0.0, 1.0)
         ax.set_xticks(range(10))
         if labels:
-            ax.set_xticklabels(labels, rotation=45)
+            ax.set_xticklabels(labels, rotation=45, ha='right')
         ax.set_yticks([0.0, 0.25, 0.5, 0.75, 1.0])
         ax.set_ylabel('Confidence')
         ax.set_xlabel('Number')
