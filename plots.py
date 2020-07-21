316a6e164a84daedd53ee732ded1083ebe376168
deokdecef44cnsnddos9ndam <68249075+deokdecef44cnsnddos9ndam@users.noreply.github.com>
2020-07-21 11:27:32 -0400

    Update plots.py

--- plots.py
+++ plots.py
@@ -123,15 +123,31 @@ def example(img, probs, label_str=None):
     
 def progress(img, model, loss_history, label_str=None):
     probs = model(img).cpu()
-    plt.close()
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(30,5))
-    if label_str:
-        label_str = f'Label: {label_str}'
-    ut.show_on_axis(ax1, img.repeat(1, 3, 1, 1).cpu(), label_str)
-    vis_probs(ax2, probs)
-    ax3.set_title('Loss')
-    ax3.set_ylabel('Loss Value')
-    ax3.set_xlabel('Iteration')
-    ax3.plot(loss_history)
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
+    if probs.shape[-1] == 10:
+        #mnist
+        plt.close()
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(30,5))
+        if label_str:
+            label_str = f'Label: {label_str}'
+        ut.show_on_axis(ax1, img.repeat(1, 3, 1, 1).cpu(), label_str)
+        vis_probs(ax2, probs)
+        ax3.set_title('Loss')
+        ax3.set_ylabel('Loss Value')
+        ax3.set_xlabel('Iteration')
+        ax3.plot(loss_history)
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+    else:
+        #imagenet
+         plt.close()
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(30,5))
+        if label_str:
+            label_str = f'Label: {label_str}'
+        ut.show_on_axis(ax1, img.cpu(), label_str)
+        vis_probs(ax2, probs)
+        ax3.set_title('Loss')
+        ax3.set_ylabel('Loss Value')
+        ax3.set_xlabel('Iteration')
+        ax3.plot(loss_history)
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
